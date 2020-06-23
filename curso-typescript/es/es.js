fd6c96d6d2b856a7bb3e0c0d98158d2155f815ec
Lucas <lucas@smcaetano.com>
2020-06-23 08:33:45 -0300

    adicionado exercicios;

--- curso-typescript/es/es.js
+++ curso-typescript/es/es.js
@@ -6,4 +6,18 @@ if (estaFrio) {
     // var acao = 'Colocar casaco';
     let acao = 'Colocar casaco';
 }
-console.log(acao);
+//console.log(acao);
+//destructing array
+const caracteristica = ['Motor', 2020];
+// const motor = caracteristica[0]
+// const ano = caracteristica[1]
+const [motor, ano] = caracteristica;
+console.log(motor);
+console.log(ano);
+const item = {
+    nome: 'SSD 400GB',
+    preco: 200
+};
+const { nome: n, preco } = item;
+console.log(n);
+console.log(preco);

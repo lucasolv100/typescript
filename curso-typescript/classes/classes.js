0a6fe85eff009b820ce2041155f05eeeec147db8
Lucas <lucas@smcaetano.com>
2020-06-24 09:25:11 -0300

    classes

--- curso-typescript/classes/classes.js
+++ curso-typescript/classes/classes.js
@@ -39,3 +39,46 @@ const produto1 = new Produto('Feijão', 5.00);
 const produto2 = new Produto('Arroz', 8.00, 0.15);
 console.log("produto1.resumo()", produto1.resumo());
 console.log("produto2.resumo()", produto2.resumo());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('Ford', 'Ka', 185);
+Array(50).fill(0).forEach(() => console.log(carro1.acelerar()));
+class Ferrari extends Carro {
+    constructor(modeloa, velocidadeMaximaa) {
+        super('Ferrari', modeloa, velocidadeMaximaa);
+        this.modeloa = modeloa;
+        this.velocidadeMaximaa = velocidadeMaximaa;
+    }
+    acelerar() {
+        return this.alterarVelocidade(30);
+    }
+    frear() {
+        return this.alterarVelocidade(-25);
+    }
+}
+const ferr = new Ferrari('F40', 350);
+console.log(ferr.acelerar());
+console.log(ferr.frear());
--- curso-typescript/classes/classes.ts
+++ curso-typescript/classes/classes.ts
@@ -50,3 +50,61 @@ const produto2 = new Produto('Arroz', 8.00, 0.15);
 
 console.log("produto1.resumo()", produto1.resumo())
 console.log("produto2.resumo()", produto2.resumo())
+
+
+class Carro {
+
+    private velocidadeAtual: number = 0;
+    constructor(public marca:string, public modelo: string, private velocidadeMaxima: number = 200) {
+
+    }
+
+    protected alterarVelocidade(delta: number) : number {
+        const novaVelocidade = this.velocidadeAtual + delta
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+
+        if(velocidadeValida){
+            this.velocidadeAtual = novaVelocidade
+        }
+        else {
+            this.velocidadeAtual =  delta > 0 ? this.velocidadeMaxima : 0;
+        }
+
+        return this.velocidadeAtual;
+    }
+
+    public acelerar() : number {
+        return this.alterarVelocidade(5)
+    }
+
+    public frear() : number {
+        return this.alterarVelocidade(-5)
+    }
+
+}
+
+const carro1 = new Carro('Ford', 'Ka', 185);
+
+Array(50).fill(0).forEach( () => console.log(carro1.acelerar()));
+
+class Ferrari extends Carro {
+
+    constructor(public modeloa: string, private velocidadeMaximaa: number) {
+        super('Ferrari', modeloa, velocidadeMaximaa)
+    }
+
+
+    public acelerar() : number {
+        return this.alterarVelocidade(30)
+    }
+
+    public frear() : number {
+        return this.alterarVelocidade(-25)
+    }
+}
+
+
+const ferr = new Ferrari('F40', 350);
+console.log(ferr.acelerar())
+console.log(ferr.frear())
+
